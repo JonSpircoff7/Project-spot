f939a541f519255dfd3a29624c7eeb1db2ee24b5
Jonathan <Spircoff_j@yahoo.com>
2022-11-14 13:24:05 -0500

    added terms page with route and fixed login/homepage/signup css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -89,4 +89,14 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+router.get("/terms", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("terms");
+});
+
 module.exports = router;
