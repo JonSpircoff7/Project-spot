92945272c09bf54dc1db42ff7b7b1271072c4508
Jonathan <Spircoff_j@yahoo.com>
2022-11-15 11:06:17 -0500

    Fixed database issues.

--- models/Project.js
+++ models/Project.js
@@ -5,34 +5,59 @@ class Project extends Model {}
 
 Project.init(
   {
-    name: {
-      type: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    description: {
-      type: DataTypes.STRING,
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING },
+    needed_funding: { type: DataTypes.FLOAT, allowNull: false },
+    pledged: { type: DataTypes.INTEGER, allowNull: false },
+    currency: { type: DataTypes.STRING },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    state_changed_at: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
+
+    launched_at: {
+      type: DataTypes.DATE,
       allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
+
+    backers_count: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_month: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_day: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_hr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    create_to_launch: { type: DataTypes.STRING, allowNull: false },
+    launch_to_deadline: { type: DataTypes.STRING, allowNull: false },
+    create_to_launch_days: { type: DataTypes.INTEGER, allowNull: false },
+    launch_to_deadline_days: { type: DataTypes.INTEGER, allowNull: false },
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
     underscored: true,
     modelName: "project",
   }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -16,25 +16,50 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
-      type: DataTypes.STRING,
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING },
+    needed_funding: { type: DataTypes.FLOAT, allowNull: false },
+    pledged: { type: DataTypes.INTEGER, allowNull: false },
+    currency: { type: DataTypes.STRING },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    state_changed_at: {
+      type: DataTypes.DATE,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
+    date_created: {
+      type: DataTypes.DATE,
       allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+      defaultValue: DataTypes.NOW,
     },
-    password: {
-      type: DataTypes.STRING,
+
+    launched_at: {
+      type: DataTypes.DATE,
       allowNull: false,
-      validate: {
-        len: [8],
-      },
+      defaultValue: DataTypes.NOW,
     },
+
+    backers_count: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_month: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_day: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_hr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    create_to_launch: { type: DataTypes.STRING, allowNull: false },
+    launch_to_deadline: { type: DataTypes.STRING, allowNull: false },
+    create_to_launch_days: { type: DataTypes.INTEGER, allowNull: false },
+    launch_to_deadline_days: { type: DataTypes.INTEGER, allowNull: false },
   },
   {
     hooks: {
@@ -43,7 +68,10 @@ User.init(
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
         return updatedUserData;
       },
     },
@@ -51,7 +79,7 @@ User.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
