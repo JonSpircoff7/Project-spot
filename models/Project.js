651692ecd9cc87e1186b67ae79ccdbbef08728df
sbullocks <sbullocks@gmail.com>
2022-11-15 12:16:36 -0500

    added the id

--- models/Project.js
+++ models/Project.js
@@ -2,3 +2,12 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Project extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
