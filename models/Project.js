88dd89d6215067dd899b31ef6b1a7d57b193ff02
Curran Duke <cdsonics@yahoo.com>
2022-11-15 11:18:56 -0500

    changed model around to account for csv

--- models/Project.js
+++ models/Project.js
@@ -11,124 +11,48 @@ Project.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-    created_at: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      // defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-    pledged: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-
-    },
-    currency: {
-      type: DataTypes.STRING,
-    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING },
+    needed_funding: { type: DataTypes.FLOAT, allowNull: false },
+    pledged: { type: DataTypes.INTEGER, allowNull: false },
+    currency: { type: DataTypes.STRING },
     deadline: {
-      type: DataTypes.STRING,
+      type: DataTypes.DATE,
       allowNull: false,
-      // defaultValue: DataTypes.NOW,
+      defaultValue: DataTypes.NOW,
     },
     state_changed_at: {
-      type: DataTypes.STRING,
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    date_created: {
+      type: DataTypes.DATE,
       allowNull: false,
-      // defaultValue: DataTypes.NOW,
+      defaultValue: DataTypes.NOW,
     },
     launched_at: {
-      type: DataTypes.STRING,
+      type: DataTypes.DATE,
       allowNull: false,
-      // defaultValue: DataTypes.NOW,
+      defaultValue: DataTypes.NOW,
     },
-    backers_count: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    deadline_month: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    deadline_day: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    deadline_hr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    state_changed_at_yr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    state_changed_at_hr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    created_at_month: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    created_at_day: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    created_at_yr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    created_at_hr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    launched_at_month: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    launched_at_day: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    launched_at_yr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    launched_at_hr: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    create_to_launch: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    launch_to_deadline: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    create_to_launch_days: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
-    launch_to_deadline_days: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-    },
+    backers_count: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_month: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_day: { type: DataTypes.INTEGER, allowNull: false },
+    deadline_hr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    state_changed_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    created_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_month: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_day: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_yr: { type: DataTypes.INTEGER, allowNull: false },
+    launched_at_hr: { type: DataTypes.INTEGER, allowNull: false },
+    create_to_launch: { type: DataTypes.STRING, allowNull: false },
+    launch_to_deadline: { type: DataTypes.STRING, allowNull: false },
+    create_to_launch_days: { type: DataTypes.INTEGER, allowNull: false },
+    launch_to_deadline_days: { type: DataTypes.INTEGER, allowNull: false },
   },
   {
     sequelize,
