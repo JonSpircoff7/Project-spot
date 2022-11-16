f8d876c42acce8693b032cb89bb9234a1e4a40a6
Curran Duke <cdsonics@yahoo.com>
2022-11-16 11:00:06 -0500

    added donate button functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,7 +8,11 @@ const newFormHandler = async (event) => {
   if (name && needed_funding && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({
+         name,
+         needed_funding,
+         description,
+       }),
       headers: {
         'Content-Type': 'application/json',
       },
